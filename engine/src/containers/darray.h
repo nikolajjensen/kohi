0748cc00ad2f4747dd05f8824a48542ccd45b39f
Nikolaj B. Jensen <nikko4913@icloud.com>
2025-02-01 13:24:19 +0000

    Video #009

--- engine/src/containers/darray.h
+++ engine/src/containers/darray.h
@@ -77,7 +77,3 @@ KAPI void* _darray_insert_at(void* array, u64 index, void* value_ptr);
 
 #define darray_length_set(array, value) \
     _darray_field_set(array, DARRAY_LENGTH, value)
-<<<<<<< HEAD
-=======
-    
->>>>>>> 9e45280 (Event system and darray)
--- engine/src/core/application.c
+++ engine/src/core/application.c
@@ -6,6 +6,7 @@
 #include "platform/platform.h"
 #include "core/kmemory.h"
 #include "core/event.h"
+#include "core/input.h"
 
 typedef struct application_state {
     game* game_inst;
@@ -30,6 +31,7 @@ b8 application_create(game* game_inst) {
 
     // Initialize subsystems.
     initialize_logging();
+    input_initialize();
 
     // TODO: Remove this
     KFATAL("A test message: %f", 3.14f);
@@ -91,12 +93,15 @@ b8 application_run() {
                 app_state.is_running = FALSE;
                 break;
             }
+
+            input_update(0);
         }
     }
 
     app_state.is_running = FALSE;
 
     event_shutdown();
+    input_shutdown();
 
     platform_shutdown(&app_state.platform);
 
